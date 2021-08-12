42c2c00f5baba3317131638d5f356c59fb675099
mykha <efimenko.mykhailo@gmail.com>
2021-08-12 21:03:40 +0300

    Fliating lable field and button

--- Gemfile
+++ Gemfile
@@ -2,4 +2,4 @@ source 'https://rubygems.org'
 
 gem 'sinatra'
 gem 'sinatra-reloader'
-gem 'bootstrap', '~> 4.3.1'
+gem 'bootstrap'
--- main.rb
+++ main.rb
@@ -4,9 +4,3 @@ require 'sinatra/reloader'
 get '/' do
   erb :index
 end
-
-get '/about' do
-  @message = "There should be information about us"
-  erb :message
-  #erb "<div class=\"jumbotron text-center\"> About us information </div><h1></h1>"
-end
